c35aa7177b3c73bb567594f094faaa65d93d1697
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-09-25 14:53:39 -0300

    feat: create avatar component

--- ignite-feed/src/components/Post.jsx
+++ ignite-feed/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/pdro-lucas.png"
-          />
+          <Avatar src="https://github.com/pdro-lucas.png" />
           <div className={styles.authorInfo}>
             <strong>Pedro Lucas</strong>
             <span>Web Developer</span>
--- ignite-feed/src/components/Sidebar.jsx
+++ ignite-feed/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 import { PencilLine } from 'phosphor-react';
 
@@ -10,10 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/pdro-lucas.png"
-        />
+        <Avatar src="https://github.com/rocketseat.png" />
         <strong>Pedro Lucas</strong>
         <span>Web Developer</span>
 
