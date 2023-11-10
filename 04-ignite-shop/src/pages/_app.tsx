96bc45616733d10aefcad3502a4734e61959a9d8
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-10 14:40:32 -0300

    feat: Add logo to header

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Logo from '@/assets/logo.svg';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Image from 'next/image';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto">
+      <header className="p-8">
+        <Image src={Logo} alt="" width={130} height={52} />
+      </header>
+      <Component {...pageProps} />
+    </div>
+  );
 }
