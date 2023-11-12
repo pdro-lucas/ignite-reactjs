714a71c233cb740043fae120f4fc9af34c67633e
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-12 18:59:08 -0300

    feat: Update header and main layou

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -11,11 +11,11 @@ const roboto = Roboto({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="container mx-auto">
-      <header className="p-8">
+    <div className="flex flex-col items-start justify-center min-h-screen">
+      <header className="py-8 w-full max-w-[1180px] mx-auto">
         <Image src={Logo} alt="" width={130} height={52} />
       </header>
-      <main className={`${roboto.className}`}>
+      <main className={`${roboto.className} w-full`}>
         <Component {...pageProps} />
       </main>
     </div>
