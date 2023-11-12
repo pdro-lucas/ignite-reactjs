8273b900194e3b3db7e7a612ab62c85902286ccb
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-12 16:44:12 -0300

    feat: Add Roboto font

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import Logo from '@/assets/logo.svg';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Roboto } from 'next/font/google';
 import Image from 'next/image';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,7 +15,9 @@ export default function App({ Component, pageProps }: AppProps) {
       <header className="p-8">
         <Image src={Logo} alt="" width={130} height={52} />
       </header>
-      <Component {...pageProps} />
+      <main className={`${roboto.className}`}>
+        <Component {...pageProps} />
+      </main>
     </div>
   );
 }
