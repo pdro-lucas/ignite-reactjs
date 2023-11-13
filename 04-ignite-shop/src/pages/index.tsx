6273febb6f11b44df0ec4e4f1924a1e943f123f2
pdro-lucas <pdrolucas.dev@gmail.com>
2023-11-12 22:12:15 -0300

    feat: Add product data to home page slider

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,13 +1,21 @@
-import Image from 'next/image';
-import Shirt2 from '@/assets/2.png';
-import Shirt3 from '@/assets/3.png';
-import Shirt4 from '@/assets/4.png';
-
-import { useKeenSlider } from 'keen-slider/react';
+import { stripe } from '@/lib/stripe';
 import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
 import { useState } from 'react';
+import Stripe from 'stripe';
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
+}
+
+export default function Home({ products }: HomeProps) {
   const [isDragging, setIsDragging] = useState(false);
   const [sliderRef] = useKeenSlider({
     slides: {
@@ -25,74 +33,54 @@ export default function Home() {
       }`}
       ref={sliderRef}
     >
-      <a
-        href="#"
-        className="relative flex items-center justify-center p-1 overflow-hidden rounded-lg cursor-pointer keen-slider__slide bg-product-gradient group"
-      >
-        <Image
-          src={Shirt2}
-          alt=""
-          width={520}
-          height={480}
-          className="object-cover"
-        />
+      {products.map((product) => {
+        return (
+          <a
+            key={product.id}
+            href="#"
+            className="relative flex items-center justify-center p-1 overflow-hidden rounded-lg cursor-pointer keen-slider__slide bg-product-gradient group"
+          >
+            <Image
+              src={product.imageUrl}
+              alt=""
+              width={520}
+              height={480}
+              className="object-cover"
+            />
 
-        <footer className="absolute flex items-center justify-between p-8 transform translate-y-[110%] rounded-md bottom-1 left-1 right-1 bg-black/60 opacity-0 transition-all ease-in-out duration-200 group-hover:translate-y-0 group-hover:opacity-100">
-          <strong className="text-xl">Camiseta x</strong>
-          <span className="text-2xl font-bold text-green-500">R$ 79,90</span>
-        </footer>
-      </a>
-      <a
-        href="#"
-        className="relative flex items-center justify-center p-1 overflow-hidden rounded-lg cursor-pointer keen-slider__slide bg-product-gradient group"
-      >
-        <Image
-          src={Shirt3}
-          alt=""
-          width={520}
-          height={480}
-          className="object-cover"
-        />
-
-        <footer className="absolute flex items-center justify-between p-8 transform translate-y-[110%] rounded-md bottom-1 left-1 right-1 bg-black/60 opacity-0 transition-all ease-in-out duration-200 group-hover:translate-y-0 group-hover:opacity-100">
-          <strong className="text-xl">Camiseta x</strong>
-          <span className="text-2xl font-bold text-green-500">R$ 79,90</span>
-        </footer>
-      </a>
-      <a
-        href="#"
-        className="relative flex items-center justify-center p-1 overflow-hidden rounded-lg cursor-pointer keen-slider__slide bg-product-gradient group"
-      >
-        <Image
-          src={Shirt4}
-          alt=""
-          width={520}
-          height={480}
-          className="object-cover"
-        />
-
-        <footer className="absolute flex items-center justify-between p-8 transform translate-y-[110%] rounded-md bottom-1 left-1 right-1 bg-black/60 opacity-0 transition-all ease-in-out duration-200 group-hover:translate-y-0 group-hover:opacity-100">
-          <strong className="text-xl">Camiseta x</strong>
-          <span className="text-2xl font-bold text-green-500">R$ 79,90</span>
-        </footer>
-      </a>
-      <a
-        href="#"
-        className="relative flex items-center justify-center p-1 overflow-hidden rounded-lg cursor-pointer keen-slider__slide bg-product-gradient group"
-      >
-        <Image
-          src={Shirt4}
-          alt=""
-          width={520}
-          height={480}
-          className="object-cover"
-        />
-
-        <footer className="absolute flex items-center justify-between p-8 transform translate-y-[110%] rounded-md bottom-1 left-1 right-1 bg-black/60 opacity-0 transition-all ease-in-out duration-200 group-hover:translate-y-0 group-hover:opacity-100">
-          <strong className="text-xl">Camiseta x</strong>
-          <span className="text-2xl font-bold text-green-500">R$ 79,90</span>
-        </footer>
-      </a>
+            <footer className="absolute flex items-center justify-between p-8 transform translate-y-[110%] rounded-md bottom-1 left-1 right-1 bg-black/60 opacity-0 transition-all ease-in-out duration-200 group-hover:translate-y-0 group-hover:opacity-100">
+              <strong className="text-xl">{product.name}</strong>
+              <span className="text-2xl font-bold text-green-500">
+                R$ {product.price}
+              </span>
+            </footer>
+          </a>
+        );
+      })}
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  });
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount ? price.unit_amount / 100 : 0,
+    };
+  });
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
+  };
+};
