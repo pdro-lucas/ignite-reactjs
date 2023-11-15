ec61fe01a41155b56d5b65d27d6ead952b7d6ad7
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-15 14:53:59 -0300

    feat: Add input validation and error handling to checkout API

--- 04-ignite-shop/src/pages/api/checkout.ts
+++ 04-ignite-shop/src/pages/api/checkout.ts
@@ -5,7 +5,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const priceId = 'price_1OBnhOEgISdAuNkWmNsVnX7n';
+  const { priceId } = req.body;
+
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST');
+    return res.status(405).end('Method not allowed');
+  }
+
+  if (!priceId) {
+    return res.status(400).json({
+      error: 'Missing priceId',
+    });
+  }
+
   const successUrl = `${process.env.NEXT_URL}/success`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
