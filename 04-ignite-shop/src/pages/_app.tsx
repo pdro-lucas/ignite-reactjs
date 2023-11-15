c5fb7c10f86aed9a94118c2e02a354488025c8d0
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-15 16:20:21 -0300

    feat: Add link to home page in logo header

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Roboto } from 'next/font/google';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -13,7 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex flex-col items-start justify-center min-h-screen">
       <header className="py-8 w-full max-w-[1180px] mx-auto">
-        <Image src={Logo} alt="" width={130} height={52} />
+        <Link href="/">
+          <Image src={Logo} alt="" width={130} height={52} />
+        </Link>
       </header>
       <main className={`${roboto.className} w-full`}>
         <Component {...pageProps} />
