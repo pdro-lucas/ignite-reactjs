75a8278af94567570e01a4f324ae9977140ee019
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-15 16:13:26 -0300

    feat: Add Stripe integration and success page

--- 04-ignite-shop/src/pages/success.tsx
+++ 04-ignite-shop/src/pages/success.tsx
@@ -1,16 +1,27 @@
+import { stripe } from '@/lib/stripe';
+import { GetServerSideProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import Stripe from 'stripe';
 
-export default function Success() {
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    image: string;
+  };
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <div className="max-w-[1180px] mx-auto flex flex-col items-center justify-center h-[656px]">
       <h1 className="text-4xl font-bold text-zinc-300">Compra efetuada</h1>
       <div className="w-full max-w-[130px] h-36 rounded-lg p-1 flex items-center justify-center bg-product-gradient mt-16">
-        {/* <Image src="/img/success.png" width={500} height={500} alt=''/> */}
+        <Image src={product.image} width={120} height={110} alt="" />
       </div>
       <p className="max-w-xl mt-8 text-2xl text-center text-zinc-200">
-        Uhuul <strong>Pedro Lucas</strong>, sua{' '}
-        <strong>Camiseta Beyound The Limits</strong> está a caminho da sua casa!
+        Uhuul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> está a caminho da sua casa!
       </p>
 
       <Link
@@ -22,3 +33,33 @@ export default function Success() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id);
+
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  });
+
+  const customerName = session.customer_details?.name;
+  const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        image: product.images[0],
+      },
+    },
+  };
+};
