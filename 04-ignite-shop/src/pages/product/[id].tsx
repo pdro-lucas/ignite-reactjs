90bf50143b74f42ed54069e5806d2d19cb6e6424
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-11-15 14:04:16 -0300

    feat: Add product details and Stripe integration

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,30 +1,43 @@
-import { useRouter } from 'next/router';
+import { stripe } from '@/lib/stripe';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import Stripe from 'stripe';
 
-export default function Product() {
-  const router = useRouter();
-  const { id } = router.query;
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    description: string;
+    imageUrl: string;
+    price: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <p>Carregando...</p>;
+  }
 
   return (
     <div className="grid grid-cols-2 max-w-[1180px] mx-auto items-stretch gap-16">
       <div className="flex items-center justify-center w-full max-w-xl p-2 rounded-lg bg-product-gradient h-[656px]">
         <Image
           className="object-cover"
-          src="https://files.stripe.com/links/MDB8YWNjdF8xT0JsaGxFZ0lTZEF1TmtXfGZsX3Rlc3RfVDZPUEFHc1N4RTFDQU1pbEo2TVVSdzh500OHQgS5dS"
+          src={product.imageUrl}
           alt="Picture of the author"
           width={520}
           height={520}
         />
       </div>
       <div className="flex flex-col">
-        <h1 className="text-2xl font-bold text-zinc-300">Camiseta X</h1>
-        <span className="block mt-4 text-2xl text-emerald-500">R$ 79,99</span>
-        <p className="mt-10 text-lg/7 text-zinc-400">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et vel
-          expedita cum iusto suscipit pariatur corporis temporibus, accusantium,
-          facere amet rem. Voluptatum voluptatibus rerum labore aperiam aliquam,
-          illo impedit eius?
-        </p>
+        <h1 className="text-2xl font-bold text-zinc-300">{product.name}</h1>
+        <span className="block mt-4 text-2xl text-emerald-500">
+          {product.price}
+        </span>
+        <p className="mt-10 text-lg/7 text-zinc-400">{product.description}</p>
         <button className="p-5 mt-auto text-lg font-bold text-white transition-colors border-none rounded-lg cursor-pointer bg-emerald-500 hover:bg-emerald-600">
           Comprar agora
         </button>
@@ -32,3 +45,48 @@ export default function Product() {
     </div>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: 'prod_OznHIsNl1EeuRe' },
+      },
+    ],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  if (!params || !params.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount ? price.unit_amount / 100 : 0),
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
+};
