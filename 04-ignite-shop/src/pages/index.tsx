c8e36202f135c42a9098c2324d3ae334b025e017
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-21 13:57:26 -0300

    refactor: add product slider

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,80 +1,67 @@
-import { stripe } from '@/lib/stripe';
-import 'keen-slider/keen-slider.min.css';
-import { useKeenSlider } from 'keen-slider/react';
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
-import Stripe from 'stripe';
+import { Slider } from '@/components/ProductsSlider'
+import SliderArrow from '@/components/ProductsSlider/SliderArrow'
+import { stripe } from '@/lib/stripe'
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import { useState } from 'react'
+import Stripe from 'stripe'
 
-interface HomeProps {
+export interface ProductProps {
   products: {
-    id: string;
-    name: string;
-    imageUrl: string;
-    price: string;
-  }[];
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+  }[]
 }
 
-export default function Home({ products }: HomeProps) {
-  const [isDragging, setIsDragging] = useState(false);
-  const [sliderRef] = useKeenSlider({
+export default function Home({ products }: ProductProps) {
+  const [isDragging, setIsDragging] = useState(false)
+  const [sliderRef, instanceRef] = useKeenSlider({
     slides: {
       perView: 3,
       spacing: 48,
     },
     dragStarted: () => setIsDragging(true),
     dragEnded: () => setIsDragging(false),
-  });
+  })
 
   return (
     <>
       <Head>
         <title>Home | IgShop</title>
       </Head>
-      <div
-        className={`flex max-w-[calc(100vw-((100vw-1180px)/2))] ml-auto min-h-[656px] keen-slider ${
-          isDragging ? 'cursor-grabbing' : 'cursor-grab'
-        }`}
-        ref={sliderRef}
-      >
-        {products.map((product) => {
-          return (
-            <Link
-              key={product.id}
-              href={`/product/${product.id}`}
-              className="relative flex items-center justify-center p-1 overflow-hidden rounded-lg cursor-pointer keen-slider__slide bg-product-gradient group"
-            >
-              <Image
-                src={product.imageUrl}
-                alt=""
-                width={520}
-                height={480}
-                className="object-cover"
-              />
 
-              <footer className="absolute flex items-center justify-between p-8 transform translate-y-[110%] rounded-md bottom-1 left-1 right-1 bg-black/60 opacity-0 transition-all ease-in-out duration-200 group-hover:translate-y-0 group-hover:opacity-100">
-                <strong className="text-xl">{product.name}</strong>
-                <span className="text-2xl font-bold text-emerald-500">
-                  {product.price}
-                </span>
-              </footer>
-            </Link>
-          );
-        })}
-      </div>
+      <SliderArrow
+        direction="left"
+        onClick={(e) => {
+          e.stopPropagation()
+          instanceRef.current?.prev()
+        }}
+      />
+
+      <SliderArrow
+        direction="right"
+        onClick={(e) => {
+          e.stopPropagation()
+          instanceRef.current?.next()
+        }}
+      />
+
+      <Slider products={products} />
     </>
-  );
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
-  });
+  })
 
   const products = response.data.map((product) => {
-    const price = product.default_price as Stripe.Price;
+    const price = product.default_price as Stripe.Price
 
     return {
       id: product.id,
@@ -84,13 +71,13 @@ export const getStaticProps: GetStaticProps = async () => {
         style: 'currency',
         currency: 'BRL',
       }).format(price.unit_amount ? price.unit_amount / 100 : 0),
-    };
-  });
+    }
+  })
 
   return {
     props: {
       products,
     },
     revalidate: 60 * 60 * 2, // 2 hours
-  };
-};
+  }
+}
