1bdcddd09715e1be731f60ce6c4fe5a26b341173
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-21 10:52:34 -0300

    feat: add error handling for missing secret key

--- 04-ignite-shop/src/lib/stripe.ts
+++ 04-ignite-shop/src/lib/stripe.ts
@@ -1,9 +1,15 @@
-import Stripe from 'stripe';
+import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY
+
+if (!STRIPE_SECRET_KEY) {
+  throw new Error('The STRIPE_SECRET_KEY environment variable is not defined')
+}
+
+export const stripe = new Stripe(STRIPE_SECRET_KEY, {
   apiVersion: '2023-10-16',
   appInfo: {
     name: 'Igshop',
     version: '0.1.0',
   },
-});
+})
