dfe599cfba0695d1ead47aadbb0000b30d749ded
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-21 15:43:18 -0300

    feat: Add ShoppingCart component to header

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Logo from '@/assets/logo.svg'
+import { ShoppingCart } from '@/components/ShoppingCart'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
@@ -12,8 +13,8 @@ const roboto = Roboto({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="flex flex-col items-start justify-start gap-20 min-h-screen">
-      <header className="py-8 max-w-[calc(100vw-((100vw-1180px)/2))] w-full mx-auto">
+    <div className="flex flex-col items-start justify-start min-h-screen gap-20">
+      <header className="py-8 flex justify-between items-center max-w-[calc(100vw-((100vw-1180px)/2))] w-full mx-auto">
         <Link href="/">
           <Image
             src={Logo}
@@ -22,6 +23,8 @@ export default function App({ Component, pageProps }: AppProps) {
             height={52}
           />
         </Link>
+
+        <ShoppingCart />
       </header>
       <main className={`${roboto.className} w-full overflow-hidden`}>
         <Component {...pageProps} />
