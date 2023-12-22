126e71a0d8fada7a0d49a3067504280b0ccc4c21
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-22 10:35:42 -0300

    refactor: use Navbar component

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,10 +1,7 @@
-import Logo from '@/assets/logo.svg'
-import { ShoppingCart } from '@/components/ShoppingCart'
+import { Navbar } from '@/components/Navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
-import Image from 'next/image'
-import Link from 'next/link'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -16,18 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div
       className={`${roboto.className} flex flex-col items-start justify-start min-h-screen gap-20`}
     >
-      <header className="py-8 flex justify-between items-center max-w-[calc(100vw-((100vw-1180px)/2))] w-full mx-auto">
-        <Link href="/">
-          <Image
-            src={Logo}
-            alt=""
-            width={130}
-            height={52}
-          />
-        </Link>
-
-        <ShoppingCart />
-      </header>
+      <Navbar />
       <main className="w-full overflow-hidden">
         <Component {...pageProps} />
       </main>
