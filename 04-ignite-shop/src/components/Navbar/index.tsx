7f3d16a21ed04795fa194dd38b47a9fde8b784fe
pdro-lucas <pdrolucas.dev@gmail.com>
2023-12-22 16:03:29 -0300

    feat: Add shopping cart dialog to navbar

--- 04-ignite-shop/src/components/Navbar/index.tsx
+++ 04-ignite-shop/src/components/Navbar/index.tsx
@@ -1,4 +1,6 @@
 import Logo from '@/assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { ShoppingCart } from '../ShoppingCart'
@@ -15,7 +17,62 @@ export function Navbar() {
         />
       </Link>
 
-      <ShoppingCart />
+      <Dialog.Root>
+        <Dialog.Trigger>
+          <ShoppingCart />
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed inset-0 z-20" />
+
+          <Dialog.Content className="data-[state=open]:animate-slideIn data-[state=closed]:animate-slideOut fixed top-0 right-0 z-30 h-[100vh] p-6 w-full max-w-[450px] bg-zinc-900 shadow-[-4px_0px_30px_0px_rgba(0,0,0,0.80)]">
+            <Dialog.Close className="w-full text-right">
+              <button className="text-zinc-400">
+                <X size={24} />
+              </button>
+            </Dialog.Close>
+
+            <Dialog.Title className="text-xl">Sacola de compras</Dialog.Title>
+            <div className="flex flex-col justify-between h-[91%] mt-8">
+              <div className="space-y-6 h-[580px] scrollbar-thumb-zinc-800 scrollbar-thumb-rounded-full scrollbar-thin overflow-y-auto">
+                <div className="flex gap-5">
+                  <div className="w-24 h-24 rounded-md bg-product-gradient"></div>
+                  <div className="space-y-2">
+                    <p className="text-lg text-zinc-200">
+                      Camiseta Beyond the Limits
+                    </p>
+                    <p className="text-lg font-bold">R$ 79,90</p>
+                    <button className="block text-emerald-600">Remover</button>
+                  </div>
+                </div>
+                <div className="flex gap-5">
+                  <div className="w-24 h-24 rounded-md bg-product-gradient"></div>
+                  <div className="space-y-2">
+                    <p className="text-lg text-zinc-200">
+                      Camiseta Beyond the Limits
+                    </p>
+                    <p className="text-lg font-bold">R$ 79,90</p>
+                    <button className="block text-emerald-600">Remover</button>
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div className="flex justify-between text-zinc-400">
+                  <span>Quantidade</span>
+                  <span>3 Items</span>
+                </div>
+                <div className="flex justify-between text-lg font-bold">
+                  <span>Valor total</span>
+                  <span>R$ 270,00</span>
+                </div>
+                <button className="w-full p-4 mt-8 transition-colors rounded-md bg-emerald-500 hover:bg-emerald-600">
+                  Finalizar compra
+                </button>
+              </div>
+            </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </header>
   )
 }
