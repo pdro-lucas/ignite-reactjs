e82e941cd668582044ec597b17d45c74e05e0063
pdro-lucas <pdrolucas.dev@gmail.com>
2024-01-02 10:54:32 -0300

    style: fix linting

--- pizzashop/src/App.tsx
+++ pizzashop/src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 
 export function App() {
   return (
-    <div className="flex flex-col gap-4 items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
       <h1>Hello World</h1>
       <Button>Click me</Button>
     </div>
