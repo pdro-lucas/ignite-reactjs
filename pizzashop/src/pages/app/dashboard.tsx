62357ab2fe0ca4b202cf874f7d2470c9b8861d54
pdro-lucas <pdrolucas.dev@gmail.com>
2024-01-02 11:32:38 -0300

    feat: Add dashboard page

---
+++ pizzashop/src/pages/app/dashboard.tsx
@@ -0,0 +1,11 @@
+export function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>
+        This is the dashboard page. You can see that it is loaded in the{' '}
+        <code>src/pages/app/dashboard.tsx</code> file.
+      </p>
+    </div>
+  )
+}
