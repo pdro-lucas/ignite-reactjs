1dcb1f17a84e1686791349770dc75113d4533d49
pdro-lucas <pdrolucas.dev@gmail.com>
2024-01-02 11:32:44 -0300

    feat: Add sign-in page

---
+++ pizzashop/src/pages/auth/sign-in.tsx
@@ -0,0 +1,11 @@
+export function SignIn() {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <p>
+        This is the sign in page. You can see that it is loaded in the{' '}
+        <code>src/pages/auth/sign-in.tsx</code> file.
+      </p>
+    </div>
+  )
+}
