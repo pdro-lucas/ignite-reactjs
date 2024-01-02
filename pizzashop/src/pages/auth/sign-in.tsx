19b52b3a9497f79e4588ddf1385d5446bd5f00e9
pdro-lucas <pdrolucas.dev@gmail.com>
2024-01-02 11:55:12 -0300

    feat: Add page titles using Helmet component

--- pizzashop/src/pages/auth/sign-in.tsx
+++ pizzashop/src/pages/auth/sign-in.tsx
@@ -1,6 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
   return (
     <div>
+      <Helmet title="Login" />
+
       <h1>Sign In</h1>
       <p>
         This is the sign in page. You can see that it is loaded in the{' '}
