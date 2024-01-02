f5a001339e0cd45a016acd4d4ab120d0953820a4
pdro-lucas <pdrolucas.dev@gmail.com>
2024-01-02 14:13:32 -0300

    feat: add base layout for the sign-in page

--- pizzashop/src/pages/auth/sign-in.tsx
+++ pizzashop/src/pages/auth/sign-in.tsx
@@ -1,15 +1,36 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
-    <div>
+    <>
       <Helmet title="Login" />
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
 
-      <h1>Sign In</h1>
-      <p>
-        This is the sign in page. You can see that it is loaded in the{' '}
-        <code>src/pages/auth/sign-in.tsx</code> file.
-      </p>
-    </div>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu E-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
