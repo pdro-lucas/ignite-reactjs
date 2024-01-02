abb94a430648c371d9c3285049943a10dec22398
pdro-lucas <pdrolucas.dev@gmail.com>
2024-01-02 00:41:52 -0300

    feat: initial config for the projet

---
+++ pizzashop/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export function App() {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
