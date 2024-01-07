bddef5c39d406f53ed5e84aa5d14895491711299
pdro-lucas <pdrolucas.dev@gmail.com>
2024-01-07 01:33:05 -0300

    feat: add dinamic colors and labels to pie graph

--- pizzashop/src/pages/app/dashboard/popular-products-chart.tsx
+++ pizzashop/src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,5 +1,5 @@
 import { BarChart } from 'lucide-react'
-import { Pie, PieChart, ResponsiveContainer } from 'recharts'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
@@ -27,6 +27,14 @@ const data = [
   },
 ]
 
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
+]
+
 export function PopularProductsChart() {
   return (
     <Card className="col-span-3">
@@ -50,8 +58,48 @@ export function PopularProductsChart() {
               cy="50%"
               outerRadius={86}
               innerRadius={64}
-              fill={colors.orange[500]}
-            />
+              strokeWidth={8}
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180
+                const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? 'start' : 'end'}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product.length > 12
+                      ? data[index].product.substring(0, 12).concat('...')
+                      : data[index].product}{' '}
+                    ({value})
+                  </text>
+                )
+              }}
+            >
+              {data.map((_, index) => {
+                return (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index]}
+                    className="stroke-background hover:opacity-80"
+                  />
+                )
+              })}
+            </Pie>
           </PieChart>
         </ResponsiveContainer>
       </CardContent>
