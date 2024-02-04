5a7697e5bab5feac332ec29a5f57e072c39aee5e
pdro-lucas <pdrolucas.dev@gmail.com>
2024-02-03 22:31:16 -0300

    feat: get profile data

---
+++ pizzashop/src/api/get-profile.ts
@@ -0,0 +1,16 @@
+import { api } from '@/lib/axios'
+
+interface getProfileResponse {
+  id: string
+  name: string
+  email: string
+  phone: string
+  role: 'manager' | 'customer'
+  createdAt: Date | null
+  updatedAt: Date | null
+}
+export async function getProfile() {
+  const response = await api.get<getProfileResponse>('/me')
+
+  return response.data
+}
