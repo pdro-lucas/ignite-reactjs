2f370e312b62712e0ef95ccbfbed2ecb6c1dbc4e
pdro-lucas <pdrolucas.dev@gmail.com>
2024-02-04 17:12:42 -0300

    refactor: import sonner from shadcn components

--- pizzashop/src/app.tsx
+++ pizzashop/src/app.tsx
@@ -1,7 +1,8 @@
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
-import { Toaster } from 'sonner'
+
+import { Toaster } from '@/components/ui/sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { queryClient } from './lib/react-query'
