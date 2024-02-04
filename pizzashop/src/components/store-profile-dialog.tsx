54faff9ef43b2dcdefe017967f55aa9c61b8a208
pdro-lucas <pdrolucas.dev@gmail.com>
2024-02-04 14:47:36 -0300

    feat: implement update function to store profile

--- pizzashop/src/components/store-profile-dialog.tsx
+++ pizzashop/src/components/store-profile-dialog.tsx
@@ -1,12 +1,15 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+import { updateProfile } from '@/api/update-profile'
 
 import { Button } from './ui/button'
 import {
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -17,26 +20,48 @@ import { Input } from './ui/input'
 import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
 
-const storeProfileSchema = z.object({
+const StoreProfileSchema = z.object({
   name: z.string().min(1),
   description: z.string(),
 })
 
-type StoreProfileForm = z.infer<typeof storeProfileSchema>
+type StoreProfileForm = z.infer<typeof StoreProfileSchema>
 
 export function StoreProfileDialog() {
   const { data: managedRestaurant } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
+    staleTime: Infinity,
   })
 
-  const { register, handleSubmit } = useForm<StoreProfileForm>({
-    resolver: zodResolver(storeProfileSchema),
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<StoreProfileForm>({
+    resolver: zodResolver(StoreProfileSchema),
     values: {
       name: managedRestaurant?.name ?? '',
       description: managedRestaurant?.description ?? '',
     },
   })
+
+  const { mutateAsync: updateProfileFn } = useMutation({
+    mutationFn: updateProfile,
+  })
+
+  async function handleUpdateProfile(data: StoreProfileForm) {
+    try {
+      await updateProfileFn({
+        name: data.name,
+        description: data.description,
+      })
+
+      toast.success('Perfil atualizado com sucesso')
+    } catch {
+      toast.error('Erro ao atualizar perfil, tente novamente')
+    }
+  }
 
   return (
     <DialogContent>
@@ -47,7 +72,7 @@ export function StoreProfileDialog() {
         </DialogDescription>
       </DialogHeader>
 
-      <form className="space-y-6">
+      <form className="space-y-6" onSubmit={handleSubmit(handleUpdateProfile)}>
         <div className="p-y-4 space-y-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-left" htmlFor="name">
@@ -68,10 +93,12 @@ export function StoreProfileDialog() {
           </div>
         </div>
         <DialogFooter>
-          <Button type="button" variant={'ghost'}>
-            Cancelar
-          </Button>
-          <Button type="submit" variant={'success'}>
+          <DialogClose asChild>
+            <Button type="button" variant={'ghost'}>
+              Cancelar
+            </Button>
+          </DialogClose>
+          <Button type="submit" variant={'success'} disabled={isSubmitting}>
             Salvar
           </Button>
         </DialogFooter>
