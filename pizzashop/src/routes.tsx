a4774aa147a92e0e6e3c7bca3016a0f93b6c1f7e
pdro-lucas <pdrolucas.dev@gmail.com>
2024-02-12 16:22:40 -0300

    feat: show error page

--- pizzashop/src/routes.tsx
+++ pizzashop/src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { NotFound } from './pages/app/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
+import { Error } from './pages/app/error'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
@@ -12,7 +13,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -37,5 +38,9 @@ export const router = createBrowserRouter([
         element: <SignUp />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
