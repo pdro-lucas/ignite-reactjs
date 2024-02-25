d1b7c2b2d1b03eb96b1c153f27288c7f20d3fd92
pdro-lucas <pdrolucas.dev@gmail.com>
2024-02-25 00:50:48 -0300

    feat: create test for sign-in page

---
+++ pizzashop/src/components/sign-in.spec.tsx
@@ -0,0 +1,29 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { HelmetProvider } from 'react-helmet-async'
+import { MemoryRouter } from 'react-router-dom'
+
+import { queryClient } from '@/lib/react-query'
+import { SignIn } from '@/pages/auth/sign-in'
+
+describe('SignIn', () => {
+  it('should set default e-mail input value if e-mail present on search params', () => {
+    const wrapper = render(<SignIn />, {
+      wrapper: ({ children }) => {
+        return (
+          <HelmetProvider>
+            <MemoryRouter initialEntries={['/sign-in?email=johndoe@email.com']}>
+              <QueryClientProvider client={queryClient}>
+                {children}
+              </QueryClientProvider>
+            </MemoryRouter>
+          </HelmetProvider>
+        )
+      },
+    })
+
+    const emailInput = wrapper.getByLabelText('Seu E-mail') as HTMLInputElement
+
+    expect(emailInput.value).toEqual('johndoe@email.com')
+  })
+})
