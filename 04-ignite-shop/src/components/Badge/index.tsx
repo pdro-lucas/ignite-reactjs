d40f4e5896b91c6e851a0f72c328995730e1e175
pdro-lucas <pdrolucas.dev@gmail.com>
2024-03-24 23:32:16 -0300

    refactor: update font-size

--- 04-ignite-shop/src/components/Badge/index.tsx
+++ 04-ignite-shop/src/components/Badge/index.tsx
@@ -5,7 +5,9 @@ interface BadgeProps {
 export function Badge({ count }: BadgeProps) {
   return (
     <span className="absolute flex items-center justify-center w-5 h-5 leading-none rounded-full outline outline-4 outline-zinc-950 -top-2 bg-emerald-600 -right-2">
-      <span className="font-bold text-white">{count}</span>
+      <span className="text-sm text-white leading-snug font-medium">
+        {count}
+      </span>
     </span>
   )
 }
