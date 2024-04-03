5159be92dc2f35acf2407119d2c588bffd4c06ff
pdro-lucas <pdrolucas.dev@gmail.com>
2024-04-02 21:06:35 -0300

    feat: Adiciona componente Box

---
+++ 05-design-system/packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,12 @@
+import { Box, BoxProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: <span>Testando o elemento box</span>,
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
