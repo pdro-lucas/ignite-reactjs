039b28a8d29f5c8974c53f2ec53b502f360d179c
pdro-lucas <pdrolucas.dev@gmail.com>
2024-04-05 13:46:54 -0300

    feat: Adiciona componente Avatar ao design system

---
+++ 05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,18 @@
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/pdro-lucas.png',
+    alt: 'John Doe',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
