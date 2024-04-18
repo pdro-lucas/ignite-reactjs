78d6167cd6c8af62e0e028c10fa873caf2088969
pdro-lucas <pdrolucas.dev@gmail.com>
2024-04-17 23:23:55 -0300

    feat: Adiciona arquivos e configurações iniciais para o projeto Ignite Call

---
+++ ignite-call/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Heading } from '@ignite-ui/react'
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <Heading as="h1">Hello World</Heading>
+      </main>
+    </>
+  )
+}
