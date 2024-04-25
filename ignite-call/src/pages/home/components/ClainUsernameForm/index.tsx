431e7103e3a24fbcabe03f16efdc3906022bec15
pdro-lucas <pdrolucas.dev@gmail.com>
2024-04-25 00:58:58 -0300

    feat: adiciona validação de username no formulário

--- ignite-call/src/pages/home/components/ClainUsernameForm/index.tsx
+++ ignite-call/src/pages/home/components/ClainUsernameForm/index.tsx
@@ -1,11 +1,34 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const clainUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3)
+    .regex(/^([a-z\\-]+)$/i)
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClainUsernameFormData = z.infer<typeof clainUsernameFormSchema>
+
 export function ClainUsernameForm() {
+  const { register, handleSubmit } = useForm<ClainUsernameFormData>({
+    resolver: zodResolver(clainUsernameFormSchema),
+  })
+
+  async function handleClainUsername(data: ClainUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClainUsername)}>
       <TextInput
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
         size="sm"
         prefix="ignite.com/"
         placeholder="Seu usuário"
