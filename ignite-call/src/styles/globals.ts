1901e09db6dd4f9d0f86e0c1ec0fc9a997d25b30
pdro-lucas <pdrolucas.dev@gmail.com>
2024-04-25 00:57:16 -0300

    fix: remove o background do input com autofill

--- ignite-call/src/styles/globals.ts
+++ ignite-call/src/styles/globals.ts
@@ -12,4 +12,12 @@ export const globalStyles = globalCss({
     color: '$gray100',
     '-webkit-font-smoothing': 'antialiased',
   },
+
+  'input:-webkit-autofill': {
+    '-webkit-animation-delay': '1s',
+    '-webkit-animation-name': 'autofill',
+    '-webkit-animation-fill-mode': 'both',
+    '-webkit-box-shadow': '0 0 0px 1000px $colors$gray900 inset',
+    '-webkit-text-fill-color': '$colors$gray100',
+  },
 })
