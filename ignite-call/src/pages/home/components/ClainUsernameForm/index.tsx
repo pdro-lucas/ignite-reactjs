3b80da67cc4027ade7f56c298ebe0978073bd417
pdro-lucas <pdrolucas.dev@gmail.com>
2024-04-27 17:29:09 -0300

    feat: adiciona validação com @zod e exibe mensagens de erro

--- ignite-call/src/pages/home/components/ClainUsernameForm/index.tsx
+++ ignite-call/src/pages/home/components/ClainUsernameForm/index.tsx
@@ -1,22 +1,28 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, TextInput } from '@ignite-ui/react'
+import { Button, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { Form } from './styles'
+import { Form, FormAnotation } from './styles'
 
 const clainUsernameFormSchema = z.object({
   username: z
     .string()
-    .min(3)
-    .regex(/^([a-z\\-]+)$/i)
+    .min(3, { message: 'O usuário deve ter no mínimo 3 caracteres' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário deve conter apenas letras e hífens',
+    })
     .transform((username) => username.toLowerCase()),
 })
 
 type ClainUsernameFormData = z.infer<typeof clainUsernameFormSchema>
 
 export function ClainUsernameForm() {
-  const { register, handleSubmit } = useForm<ClainUsernameFormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClainUsernameFormData>({
     resolver: zodResolver(clainUsernameFormSchema),
   })
 
@@ -25,25 +31,34 @@ export function ClainUsernameForm() {
   }
 
   return (
-    <Form as="form" onSubmit={handleSubmit(handleClainUsername)}>
-      <TextInput
-        onPointerEnterCapture={undefined}
-        onPointerLeaveCapture={undefined}
-        size="sm"
-        prefix="ignite.com/"
-        placeholder="Seu usuário"
-        crossOrigin=""
-        css={{
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-          whiteSpace: 'nowrap',
-        }}
-        {...register('username')}
-      />
-      <Button size="sm" type="submit">
-        Reservar usuário
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClainUsername)}>
+        <TextInput
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="Seu usuário"
+          crossOrigin=""
+          css={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            whiteSpace: 'nowrap',
+          }}
+          {...register('username')}
+        />
+        <Button size="sm" type="submit">
+          Reservar usuário
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite um nome de usuário para reservar'}
+        </Text>
+      </FormAnotation>
+    </>
   )
 }
